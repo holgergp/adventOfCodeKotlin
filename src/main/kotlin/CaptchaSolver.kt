99f8966e9908b680912e06e322cf225315115271
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-01-16 23:11:15 +0100

    Factored out solver

--- src/main/kotlin/CaptchaSolver.kt
+++ src/main/kotlin/CaptchaSolver.kt
@@ -1,7 +1,26 @@
 package main.kotlin
 class CaptchaSolver {
 
-    fun solve(input: List<String>) {
-        input.map {  }
+    data class CalculationWindow(val currentChar: String, val accumulatedComputation: Int)
+
+    fun solve(puzzleInput: List<String>): Number {
+        if (puzzleInput.size <= 1) {
+            return 0;
+        }
+        val amendedPuzzleInput = puzzleInput + listOf<String>(puzzleInput.first());
+
+        return amendedPuzzleInput.fold(
+                CalculationWindow("", 0),
+                { calculationWindow, current: String ->
+                    CalculationWindow(current, conditionalCalculate(calculationWindow, current))
+                }).accumulatedComputation;
+    }
+
+    private fun conditionalCalculate(calculationWindow: CalculationWindow, current: String): Int {
+        var newAcc: Int = calculationWindow.accumulatedComputation;
+        if (calculationWindow.currentChar == current) {
+            newAcc += current.toInt();
+        }
+        return newAcc;
     }
 }
