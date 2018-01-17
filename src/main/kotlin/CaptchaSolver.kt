8b2b2a567f3a7e134e2bde85e718176cc3cc7ab7
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-01-17 08:39:36 +0100

    Moved mappinng from String to int to separate class

--- src/main/kotlin/CaptchaSolver.kt
+++ src/main/kotlin/CaptchaSolver.kt
@@ -2,28 +2,36 @@ package main.kotlin
 
 class CaptchaSolver {
 
-    data class CalculationWindow(val previousChar: String, val accumulatedComputation: Int)
+    class Puzzle(val puzzleInput: String) {
 
-    fun solve(puzzleInput: List<String>): Number {
-        if (puzzleInput.size <= 1) {
+        val puzzleInputDigits: List<Int> = puzzleInput.map({
+            c -> c.toString().toInt() })
+    }
+
+    data class CalculationWindow(val previousDigit: Int, val accumulatedComputation: Int)
+
+    fun solve(puzzleInput: String): Number {
+        val puzzle = Puzzle(puzzleInput)
+        val puzzleInputDigits = puzzle.puzzleInputDigits
+        if (puzzleInputDigits.size <= 1) {
             return 0;
         }
-        val amendedPuzzleInput = puzzleInput + listOf<String>(puzzleInput.first());
+        val amendedPuzzleInput = puzzleInputDigits + listOf<Int>(puzzleInputDigits.first());
 
         return amendedPuzzleInput.fold(
-                CalculationWindow("", 0),
-                { calculationWindow, currentChar: String ->
-                    updateCalculationWindow(calculationWindow, currentChar)
+                CalculationWindow(0, 0),
+                { calculationWindow, currentDigit: Int ->
+                    updateCalculationWindow(calculationWindow, currentDigit)
                 }).accumulatedComputation;
     }
 
-    private fun updateCalculationWindow(calculationWindow: CalculationWindow, currentChar: String): CalculationWindow {
-        val updatedAccumulatedComputation: Int = if (calculationWindow.previousChar == currentChar) {
-            calculationWindow.accumulatedComputation + currentChar.toInt()
+    private fun updateCalculationWindow(calculationWindow: CalculationWindow, currentDigit: Int): CalculationWindow {
+        val updatedAccumulatedComputation: Int = if (calculationWindow.previousDigit == currentDigit) {
+            calculationWindow.accumulatedComputation + currentDigit
         } else {
             calculationWindow.accumulatedComputation
         }
 
-        return CalculationWindow(currentChar, updatedAccumulatedComputation)
+        return CalculationWindow(currentDigit, updatedAccumulatedComputation)
     }
 }
